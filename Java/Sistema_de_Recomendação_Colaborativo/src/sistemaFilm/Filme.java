1f4e2a35aeb7da77e70baa206f517964c71f80d9
Flavio Corcini <flaviosamora06@gmail.com>
2026-03-15 15:28:11 -0300

    inicio do trabalho, projeto java
    Add projeto java com as classes "Main", "SistemaPrincipal", e o objeto "Filme".

---
+++ Java/Sistema_de_Recomendação_Colaborativo/src/sistemaFilm/Filme.java
@@ -0,0 +1,112 @@
+package sistemaFilm;
+
+import java.util.Objects;
+
+public class Filme {
+
+	private long id; //fornecido pelo usuario
+	private String titulo;
+	private String generos; //pegar a linha toda ou é melhor fazer em array ?
+	private String diretor;
+	private int ano;
+	
+	//Construtor
+	public Filme(long id, String titulo, String generos, String diretor, int ano){
+		if (id < 0) {
+	        throw new IllegalArgumentException("ID não pode ser negativo.");
+	    }
+	    if (titulo == null || titulo.isBlank()) {
+	        throw new IllegalArgumentException("Título é obrigatório.");
+	    }
+	    if (generos == null || generos.isBlank()) {
+	    	throw new IllegalArgumentException("Genero é obrigatório.");
+	    }
+	    if (diretor == null || diretor.isBlank()) {
+	        throw new IllegalArgumentException("Diretor é obrigatório.");
+	    }
+	    if (ano < 1888) { // O primeiro filme da história é de 1888
+	        throw new IllegalArgumentException("Ano de lançamento inválido.");
+	    }
+	    
+		this.id = id;
+		this.titulo = titulo;
+		this.generos = generos;
+		this.ano = ano;
+	}
+
+	//Getters e Setters
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		if (titulo == null || titulo.isBlank()) {
+	        throw new IllegalArgumentException("Título é obrigatório.");
+	    }
+		this.titulo = titulo;
+	}
+
+	public String getGeneros() {
+		return generos;
+	}
+
+	public void setGeneros(String generos) {
+		if (generos == null || generos.isBlank()) {
+	    	throw new IllegalArgumentException("Genero é obrigatório.");
+	    }
+		this.generos = generos;
+	}
+
+	public String getDiretor() {
+		return diretor;
+	}
+
+	public void setDiretor(String diretor) {
+		if (diretor == null || diretor.isBlank()) {
+			throw new IllegalArgumentException("Diretor é obrigatório.");
+		}
+		this.diretor = diretor;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		if (ano < 1888) {
+	        throw new IllegalArgumentException("Ano de lançamento inválido.");
+	    }
+		this.ano = ano;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	//hash & equals (estudar + sobre)
+	@Override
+	public int hashCode() {
+		return Objects.hash(ano, diretor, generos, id, titulo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Filme other = (Filme) obj;
+		return ano == other.ano && Objects.equals(diretor, other.diretor) && Objects.equals(generos, other.generos)
+				&& id == other.id && Objects.equals(titulo, other.titulo);
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Filme [id=" + id + ", titulo=" + titulo + ", generos=" + generos + ", diretor=" + diretor + ", ano="
+				+ ano + "]";
+	}	
+	
+}
---
+++ Java/Sistema_de_Recomendação_Colaborativo/src/sistemaFilm/SistemaPrincipal.java
@@ -0,0 +1,5 @@
+package sistemaFilm;
+
+public class SistemaPrincipal {
+
+}
