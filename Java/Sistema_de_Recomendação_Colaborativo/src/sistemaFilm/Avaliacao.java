ff5b5536bbee0bed7d3ad749b93ba5c59cf25be5
Flavio Corcini <flaviosamora06@gmail.com>
2026-04-12 01:24:07 -0300

    add class Avaliacao

---
+++ Java/Sistema_de_Recomendação_Colaborativo/src/sistemaFilm/Avaliacao.java
@@ -0,0 +1,70 @@
+package sistemaFilm;
+
+import java.util.Objects;
+
+public class Avaliacao {
+	private long usuario;
+	private long id_filme;
+	private int nota;
+	
+	public Avaliacao(long usuario, long id_filme, int nota) {
+		setUsuario(usuario);
+		setId_filme(id_filme);
+		setUsuario(usuario);
+	}
+
+	public long getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(long usuario) {
+		if (usuario <= 0) {
+			throw new IllegalArgumentException("ID de usuário inválido: deve ser um número positivo.");
+		}
+		this.usuario = usuario;
+	}
+	
+	public long getId_filme() {
+		return id_filme;
+	}
+	public void setId_filme(long id_filme) {
+		if (id_filme <= 0) {
+			throw new IllegalArgumentException("ID do filme inválido: deve ser um número positivo.");
+		}
+		this.id_filme = id_filme;
+	}
+	
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+	    if (nota < 0 || nota > 5) {
+	        throw new IllegalArgumentException("Nota inválida: a nota deve estar entre 0 e 5.");
+	    }
+	    this.nota = nota;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(usuario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Avaliacao other = (Avaliacao) obj;
+		return id_filme == other.id_filme && nota == other.nota && usuario == other.usuario;
+	}
+
+	@Override
+	public String toString() {
+		return "avaliacao [usuario= " + usuario + ", id_filme= " + id_filme + ", nota= " + nota + "]";
+	}
+	
+}
