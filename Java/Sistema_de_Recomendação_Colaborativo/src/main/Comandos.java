9d0309215609dacf243cf29b28bda8171665f3f6
Flavio Corcini <flaviosamora06@gmail.com>
2026-04-12 16:52:21 -0300

    ADD do enum Comandos e da class Usuarios

---
+++ Java/Sistema_de_Recomendação_Colaborativo/src/main/Comandos.java
@@ -0,0 +1,26 @@
+package main;
+
+public enum Comandos {
+	SAIR("SAIR"),
+	DESCONHECIDO(""),
+	GENERO("GENERO"),
+	ANO_SUPERIOR("ANO_SUPERIOR"),
+	MEDIA_AVALIACOES("MEDIA_AVALIACOES"),
+	NAO_AVALIADOS("NAO_AVALIADOS"),
+	DIRETORES_COMUNS("DIRETORES_COMUNS");
+
+	private String comandoString;
+
+	private Comandos(String texto) {
+		this.comandoString = texto;
+	}
+
+	public static Comandos deString(String texto) {
+		for (Comandos c : Comandos.values()) {
+			if (c.comandoString.equalsIgnoreCase(texto)) {
+				return c;
+			}
+		}
+		return DESCONHECIDO;
+	}
+}
---
+++ Java/Sistema_de_Recomendação_Colaborativo/src/main/Usuario.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Usuario {
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+}
