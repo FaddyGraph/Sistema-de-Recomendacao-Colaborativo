d0ff7229cf966a9c860ebb27a72f3241dcca1d6c
Flavio Corcini <flaviosamora06@gmail.com>
2026-06-27 20:00:33 -0300

    entrega da fase 1
    Consultas Básicas

---
+++ Prolog/Sistema_de_Recomendação_Colaborativo_Prolog/main.pl
@@ -0,0 +1,22 @@
+%___ Carregar fatos:
+
+%:- [data/dataset].
+:- [data/datasetTestes]. % mais facil de ver funcionando
+
+%___ Regras 
+
+%_____ Fase 1
+filmes_por_genero(Genero, Lista)  % "filmes_por_genero(Genero, Lista)" nome do predicado com os dois argumentos 'entrada'(Genero) e 'saida'(Lista). 
+:-                                % ":-" operador 'se' o que ta na esquerda é fato se o que ta na direita tiver sucesso
+
+    findall(Titulo,                         % enconte tudo e pegue o titulo
+    filme(_, Titulo, Genero, _, _, _, _),   % mostra o que e onde, onde comparar e onde pegar (nesse caso, em filme, pegar o Titulo e comparar o Genero)
+    Lista).                                 % armazene na variavel Lista
+
+filmes_de_ano_superior(AnoAlvo, Lista) :- findall(Titulo, (filme(_, Titulo, _, _, Ano, _, _),
+    Ano > AnoAlvo), % Filtro
+    Lista).
+
+notas_do_usuario(UsuarioID, Lista) :-
+    findall(Nota, avaliacao(UsuarioID, _, Nota), Lista).
+
